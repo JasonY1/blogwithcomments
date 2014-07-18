976d1b2f369e325a7c4d8bd6b759ff80deea2782
JasonY1 <jasonh.yi@gmail.com>
2014-07-17 18:34:32 -0700

    Add a full_title helper, reviewed css and ruby in regards to rails

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,32 +9,37 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')    #verify 'Sample App' is there. 
     end
     
-    it "should have the title 'Home'" do
+    it "should have the title 'Home'" do            #verify base_title
+      visit '/static_pages/home'  
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+    it "should not have a custom page title" do     #verify | Home is not on home page
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).not_to have_title('| Home')
     end
   end
   
   
-  describe "Help page" do
+  describe "Help page" do                           #verify help pages has content Help
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
     
-    it "should have title 'Help'" do
+    it "should have title 'Help'" do                #verify Help page has title indicated below
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
   
-  describe "About page" do
+  describe "About page" do                          #verify about page has About Us on page
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
     
-    it "should have the title 'About Us'" do
+    it "should have the title 'About Us'" do        #verify about page has correct title
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
