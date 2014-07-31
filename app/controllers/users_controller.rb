4f64bbbece08c01b4e6d79f2eb30c4cad63188ce
JasonY1 <jasonh.yi@gmail.com>
2014-07-30 22:14:59 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])    #def show for show.html.erb
+    @user = User.find(params[:id])    #Show current user by finding current user data
   end
   
   def new
+    @user = User.new                  #new User for current instance
   end
+  
+  def create
+    @user = User.new(user_params)     #Create new user per params
+    if @user.save                     #if valid information provided save
+      flash[:success] = "Welcome to the Sample App!" #flash msg if success
+      redirect_to @user               # redirect to @user page
+    else
+      render 'new'                    #else render new
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,17 +5,43 @@ describe "User pages" do
   subject { page }
   
   describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+    let(:user) { FactoryGirl.create(:user) }    #Use Factorygirl for users
+    before { visit user_path(user) }            # be on user_path for user
     
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
+    it { should have_content(user.name) }       # content has user's name
+    it { should have_title(user.name) }         # title has user's name
   end
   
-  describe "signup page" do
+  describe "signup page" do                     # page test for signup page
+    before { visit signup_path }                # before signup
+    
+    it { should have_content('Sign up') }       #should have Sign up as title and content
+    it { should have_title(full_title('Sign up')) }
+  end
+  
+  describe "signup" do                          #test sign up action
+    
     before { visit signup_path }
     
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    let(:submit) { "Create my account" }        # when submit, create account
+    
+    describe "with invalid information" do      # Test what happens in an invalid information scenario
+      it "should not create a user" do          # expected result should not create user
+        expect { click_button submit }.not_to change(User, :count)
+      end                                       # should not change the total user count
+    end
+    
+    describe "with valid information" do                    #Test what happens when valid information is used
+      before do
+        fill_in "Name",         with: "Example User"        #use sample "Valid" information
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foofoo"
+        fill_in "Confirmation", with: "foofoo"
+      end
+      
+      it "should create a user" do                          # with valid information it should increase total user count
+        expect { click_button submit }.to change(User, :count).by(1)
+      end                                                   # by one for the creation of account
+    end
   end
 end
